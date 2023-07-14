ce840bdad402dcadcfff6db19739df4186b5c852
Anurav-Garg <anurav.grg@gmail.com>
2023-07-14 17:36:01 +0530

    feat: add basic playback functionality to video and audio

--- src/app/audio-page/audio-page.component.ts
+++ src/app/audio-page/audio-page.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-audio-page',
   templateUrl: './audio-page.component.html',
-  styleUrls: ['./audio-page.component.css']
+  styleUrls: ['./audio-page.component.css'],
 })
-export class AudioPageComponent {
+export class AudioPageComponent implements AfterViewInit, OnChanges {
+  @ViewChild('audio') audioRef!: ElementRef;
+  micAudio!: HTMLVideoElement;
 
+  @Input() audioDevices: MediaDeviceInfo[] = [];
+  @Output() audioDevicesChange = new EventEmitter<MediaDeviceInfo[]>();
+
+  @Output() gotPermission = new EventEmitter<void>();
+
+  async ngAfterViewInit() {
+    this.micAudio = this.audioRef.nativeElement;
+
+    if (this.audioDevices.length > 0) {
+      this.setAudioStream();
+    }
+  }
+
+  async ngOnChanges(changes: SimpleChanges) {
+    if (changes['audioDevices']) {
+      console.log('changes:', changes['audioDevices']);
+
+      if (
+        this.audioDevices.length > 0 &&
+        changes['audioDevices'].previousValue &&
+        changes['audioDevices'].previousValue.length === 0
+      ) {
+        this.setAudioStream();
+      }
+    }
+  }
+
+  async setAudioStream() {
+    try {
+      this.micAudio.srcObject = await navigator.mediaDevices.getUserMedia({
+        audio: { deviceId: { exact: this.audioDevices[0].deviceId } },
+      });
+
+      this.gotPermission.emit();
+    } catch (error: any) {
+      console.log(
+        'error while setting audio stream:',
+        error.name,
+        error.message
+      );
+    }
+  }
 }
--- src/app/video-page/video-page.component.ts
+++ src/app/video-page/video-page.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-video-page',
   templateUrl: './video-page.component.html',
-  styleUrls: ['./video-page.component.css']
+  styleUrls: ['./video-page.component.css'],
 })
-export class VideoPageComponent {
+export class VideoPageComponent implements AfterViewInit, OnChanges {
+  @ViewChild('video') videoRef!: ElementRef;
+  webcamVideo!: HTMLVideoElement;
 
+  @Input() videoDevices: MediaDeviceInfo[] = [];
+  @Output() videoDevicesChange = new EventEmitter<MediaDeviceInfo[]>();
+
+  @Output() gotPermission = new EventEmitter<void>();
+
+  async ngAfterViewInit() {
+    this.webcamVideo = this.videoRef.nativeElement;
+
+    if (this.videoDevices.length > 0) {
+      this.setVideoStream();
+    }
+  }
+
+  async ngOnChanges(changes: SimpleChanges) {
+    if (changes['videoDevices']) {
+      console.log('changes:', changes['videoDevices']);
+
+      if (
+        this.videoDevices.length > 0 &&
+        changes['videoDevices'].previousValue &&
+        changes['videoDevices'].previousValue.length === 0
+      ) {
+        this.setVideoStream();
+      }
+    }
+  }
+
+  async setVideoStream() {
+    try {
+      this.webcamVideo.srcObject = await navigator.mediaDevices.getUserMedia({
+        video: { deviceId: { exact: this.videoDevices[0].deviceId } },
+      });
+
+      this.gotPermission.emit();
+    } catch (error: any) {
+      console.log(
+        'error while setting video stream:',
+        error.name,
+        error.message
+      );
+    }
+  }
 }
