f581770b43b31beb59e76fcef985867a5b5b8846
Anurav-Garg <anurav.grg@gmail.com>
2023-07-15 13:04:55 +0530

    feat: Add components and form module to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VideoPageComponent } from './video-page/video-page.component';
+import { AudioPageComponent } from './audio-page/audio-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, VideoPageComponent, AudioPageComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
