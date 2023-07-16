98e3c24d00bcd658c9ac48b5655de3082ea3fbd1
Anurav-Garg <anurav.grg@gmail.com>
2023-07-16 16:53:31 +0530

    feat: add styling and videoResolutions import to main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { videoResolution, videoResolutions } from './videoResolutions';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent implements OnInit {
   audioNotAllowed = false;
   videoDevices: MediaDeviceInfo[] = [];
   audioDevices: MediaDeviceInfo[] = [];
+  allowedVideoResolutions: videoResolution[] = videoResolutions;
 
   async ngOnInit() {
     this.updateDevices();
